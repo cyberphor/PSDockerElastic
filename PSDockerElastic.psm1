8b037ddd557c4a8423e10eaadae4be123867efdb
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-11-07 10:47:18 -0500

    Create PSDockerElastic.psm1

---
+++ PSDockerElastic.psm1
@@ -0,0 +1,6 @@
+. $PSScriptRoot\PSDockerElastic\Clear-DockerContent.ps1
+. $PSScriptRoot\PSDockerElastic\Restart-WSL.ps1
+. $PSScriptRoot\PSDockerElastic\Set-WSLDistro.ps1
+. $PSScriptRoot\PSDockerElastic\Set-VirtualMemorySize.ps1
+. $PSScriptRoot\PSDockerElastic\Show-HostsFile.ps1
+. $PSScriptRoot\PSDockerElastic\Test-Elasticsearch.ps1
